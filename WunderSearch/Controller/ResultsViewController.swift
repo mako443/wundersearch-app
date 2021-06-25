5a061dab8553472828bbd552d2250963b343a02a
Manuel Kolmet <manuel.kolmet@gmail.com>
2021-06-25 16:25:46 +0200

    Base app running

---
+++ WunderSearch/Controller/ResultsViewController.swift
@@ -0,0 +1,86 @@
+//
+//  ResultsViewController.swift
+//  WunderSearch
+//
+//  Created by maurice on 23.06.21.
+//
+
+import UIKit
+
+private let reuseIdentifier = "ResultCellIdentifier"
+
+class ResultsViewController: UICollectionViewController {
+    let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    var searchResults: [UIImage] = []
+    let itemsPerRow: CGFloat = 3
+    
+    var searchHandler: SearchHandler = SearchHandler()
+    
+    @IBOutlet weak var searchField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Register cell classes
+        self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        
+//        searchHandler.indexPhotos()
+    }
+    
+    @IBAction func searchTapped(_ sender: UIBarButtonItem) {
+        searchResults.removeAll()
+        
+        if searchField.text != nil{
+            let resultImages = searchHandler.searchPhotos(text: searchField.text!, topK: 25)
+            searchResults.append(contentsOf: resultImages)
+        } else {
+            print("Text empty")
+        }
+        
+        self.collectionView.reloadData()
+    }
+}
+
+extension ResultsViewController {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.searchResults.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .white
+        cell.contentView.subviews.forEach({ $0.removeFromSuperview() }) // Remove old image views as cell might be re-used
+        
+        let imageView: UIImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: widthPerItem, height: widthPerItem))
+        imageView.contentMode = UIImageView.ContentMode.scaleAspectFit
+        imageView.image = searchResults[indexPath.row]
+        cell.contentView.addSubview(imageView)
+        return cell
+    }
+}
+
+extension ResultsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
---
+++ WunderSearch/Model/SearchHandler.swift
@@ -0,0 +1,209 @@
+//
+//  SearchHandler.swift
+//  WunderSearch
+//
+//  Created by maurice on 18.06.21.
+//
+
+import Foundation
+import Photos
+import UIKit
+import CoreML
+import Vision
+
+class SearchHandler {
+    let manager = PHImageManager.default()
+    var indexedImages: [String: MLMultiArray] = [:] // [localIdentifier: imageDescriptor]
+    var numberOfImages: Int = 0
+    
+    var imageEncoder: VNCoreMLModel
+    var textEncoder: TextEncoder
+    var knownWords: Dictionary<String, Int> = [:]
+    let maxEncodeWords = 32
+    
+    init(){
+        print("SearchHandler()")
+        self.imageEncoder = try! VNCoreMLModel(for: ImageEncoder().model)
+        self.textEncoder = TextEncoder()
+        
+        // Encode the test image to verify model outputs
+//        let image: UIImage = #imageLiteral(resourceName: "test-image")
+//        print(image.size)
+//        let ciImage = CIImage(cgImage: image.cgImage!)
+//        self.encodeImage(model: self.imageEncoder, image: ciImage, localIdentifier: "test-image")
+        
+        let path = Bundle.main.path(forResource: "known_words", ofType: "json")
+        if path == nil {
+            fatalError("known_words.json not found.")
+        }
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path!), options: .mappedIfSafe)
+            let jsonResult = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
+            self.knownWords = jsonResult as! Dictionary<String, Int>
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    func dotProduct(a: MLMultiArray, b: MLMultiArray) -> Float {
+        assert(a.count == b.count, "Inputs need to be of equal sizes.")
+        assert((a.dataType == b.dataType) && (a.dataType == MLMultiArrayDataType.float32), "Unexpected or mismatching input data types.")
+        
+        let ptrA = UnsafePointer<Float32>(OpaquePointer(a.dataPointer))
+        let ptrB = UnsafePointer<Float32>(OpaquePointer(b.dataPointer))
+        
+        var prod: Float = 0
+        for i in 0..<a.count{
+            prod += ptrA[i] * ptrB[i]
+        }
+        return prod
+    }
+    
+    private func getTopKPhotos(textDescriptor: MLMultiArray, k: Int = 25) -> [UIImage]{
+//        print(textDescriptor)
+        
+        var scoresDict: [String: Float] = [:] // [localIdentifier: score]
+        for (id, imageDescriptor) in self.indexedImages {
+            scoresDict[id] = self.dotProduct(a: textDescriptor, b: imageDescriptor)
+        }
+        
+        // Sort by scores descending
+        let sorted = scoresDict.sorted(by: { (keyVal1, keyVal2) -> Bool in
+            keyVal1.value > keyVal2.value
+        })
+        
+        // Extract the top-k local identifiers
+        var topKIds: [String] = []
+        for i in 0..<min(sorted.count, k){
+            topKIds.append(sorted[i].key)
+        }
+        
+        // Retrieve the images by their identifiers
+        var topKImages: [UIImage] = []
+        
+        let requestOptions = PHImageRequestOptions()
+        requestOptions.isSynchronous = true
+        requestOptions.deliveryMode = .highQualityFormat
+        
+        let results: PHFetchResult = PHAsset.fetchAssets(withLocalIdentifiers: topKIds, options: PHFetchOptions())
+        for i in 0..<results.count {
+            let asset = results.object(at: i)
+            let size = CGSize(width: 500, height: 500)
+
+            manager.requestImage(for: asset, targetSize: size, contentMode: .aspectFit, options: requestOptions) { (image, infoDict) in
+                if let image = image {
+                    topKImages.append(image)
+                }
+                else {
+                    print("Image is nil.")
+                }
+            }
+        }
+        
+        return topKImages
+    }
+    
+    func searchPhotos(text: String, topK: Int) -> [UIImage] {
+        let textSplit = text.lowercased().components(separatedBy: " ")
+        
+        do {
+            let inputArray = try MLMultiArray(shape: [1, 32], dataType: MLMultiArrayDataType.float32)
+            for i in 0..<min(textSplit.count, 32){
+                inputArray[i] = 2.0 // TODO: load actual index dict
+            }
+            
+            let output = try! textEncoder.prediction(input_1: inputArray)
+            let textDescriptor = output._64
+            print(textDescriptor.shape)
+            
+            var topKPhotos = self.getTopKPhotos(textDescriptor: textDescriptor)
+            print("Num photos: \(topKPhotos.count)")
+            return topKPhotos
+        }
+        catch {
+            print(error)
+            return []
+        }
+    }
+    
+    /// Encode an image for searching by queuing a VNCoreMLRequest and adding the image descriptor to indexedImages on completion.
+    /// TODO: Ok to queue many images? Rather queue all at once in handler.perform? Async ok?
+    /// - Parameters:
+    ///   - model: The image encoder model
+    ///   - image: The image to encode
+    ///   - localIdentifier: localIdentifier of the image to encode
+    func encodeImage(model: VNCoreMLModel, image: CIImage, localIdentifier: String){
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results = request.results as? [VNCoreMLFeatureValueObservation] else {
+                fatalError("Model failed to process image.")
+            }
+//            print(results[0].featureName)
+            guard let imageDescriptor = results[0].featureValue.multiArrayValue else {
+                fatalError("Could not access image descriptor array.")
+            }
+//            print(imageDescriptor.shape)
+            self.indexedImages[localIdentifier] = imageDescriptor
+            print("\(self.indexedImages.count) images encoded")
+            
+            if localIdentifier == "test-image" {
+                print("Test image descriptor:")
+                print(imageDescriptor)
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        do {
+            try handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+    }
+    
+    private func prepareImage(image: CIImage) {
+        
+    }
+    
+    func indexPhotos() {
+        let requestOptions = PHImageRequestOptions()
+        requestOptions.isSynchronous = false
+        requestOptions.deliveryMode = .fastFormat
+        
+        let fetchOptions = PHFetchOptions()
+        fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+        
+        let results: PHFetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+//        let ids = ["CC95F08C-88C3-4012-9D6D-64A413D254B3/L0/001", "ED7AC36B-A150-4C38-BB8C-B6D696F4F2ED/L0/001"]
+//        let results: PHFetchResult = PHAsset.fetchAssets(withLocalIdentifiers: ids, options: fetchOptions)
+
+        print("Number of photos: \(results.count)")
+        
+        if results.count > 0 {
+            self.numberOfImages = results.count
+            
+            for i in 0..<results.count {
+                let asset = results.object(at: i)
+                let size = CGSize(width: 500, height: 500)
+//                print(asset.localIdentifier)
+                
+                // Currently skipping already encoded images
+                // TODO: possibly verify a checksum to check for updated images
+                if self.indexedImages[asset.localIdentifier] != nil {
+                    print("Skipping image \(asset.localIdentifier)")
+                    continue
+                }
+
+                manager.requestImage(for: asset, targetSize: size, contentMode: .aspectFit, options: requestOptions) { (image, infoDict) in
+                    if let image = image {
+                        let ciImage = CIImage(cgImage: image.cgImage!)
+                        self.encodeImage(model: self.imageEncoder, image: ciImage, localIdentifier: asset.localIdentifier)
+                    }
+                    else {
+                        print("Image is nil.")
+                    }
+                }
+            }
+        }
+    }
+}
